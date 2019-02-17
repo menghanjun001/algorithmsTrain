b4d1f3f652acda5a580024f5f24cb8aa98072984
menghanjun001 <406228554@qq.com>
2019-02-17 21:43:27 +0800

    leetcode 2019.2.17

--- 217. 存在重复元素.py
+++ 217. 存在重复元素.py
@@ -4,13 +4,15 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
-        hashmap={}
+        hashmap = {}
         for i in nums:
-            if i not in hashmap:
-                hashmap[i]=1
-            else:
-                hashmap[i]+=1
+            hashmap[i] = hashmap.get(i, 0) + 1
         for j in hashmap:
-            if hashmap[j] >1:
+            if hashmap[j] > 1:
                 return True
         return False
+
+
+if __name__ == '__main__':
+    a = Solution()
+    print(a.containsDuplicate([1, 2, 3, 4]))
